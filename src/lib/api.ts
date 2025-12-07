626af356eed1cb99a50c0a90ac44e83cc8302997
Kim-Hwije <hwije207@gmail.com>
2025-12-08 08:19:10 +0900

    final2nd

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.PROD
-  ? 'https://maket-l0oq.onrender.com/api'
-  : 'http://localhost:5000/api'
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: false,
+  baseURL: 'https://maket-l0oq.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
